760910db500020bead9f61b7ef8c6298163612db
Magnus Nilsson <regnhundar@gmail.com>
2024-09-20 20:18:46 +0200

    fixat att meddelanden renderas ut från db och stylat enligt figma

---
+++ frontend/src/components/IconButton/IconButton.tsx
@@ -0,0 +1,12 @@
+import "./iconButton.css";
+import { Link } from "react-router-dom";
+interface Props {
+    style: string;
+    to: string;
+    aria: string;
+}
+const IconButton: React.FC<Props> = ({ style, to, aria }) => {
+    return <Link className={`icon-button icon-button-${style}`} to={to} aria-label={aria} />;
+};
+
+export default IconButton;
---
+++ frontend/src/pages/NewPostPage/NewBoardPage.tsx
@@ -0,0 +1,6 @@
+import "./newBoardPage.css";
+const NewBoardPage: React.FC = () => {
+    return <div>Your component here</div>;
+};
+
+export default NewBoardPage;
